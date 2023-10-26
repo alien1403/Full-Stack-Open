349f17db429df4a39c6b0364b0ca39e351994df8
alien1403 <hanghicelrazvanmihai@gmail.com>
2023-10-26 08:54:37 +0300

    Part 2b

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,55 +1,104 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
+import { useState } from 'react'
+import Person from '../components/Person'
 
-import Course from "../components/Course"
+const Filter = (props) => {
+  return(
+    <div>
+      filter shown with <input value={props.searchInput} onChange={props.handleSearchChange}/>
+    </div>
+  )
+}
+
+const PersonForm = ({addPerson, newName, handleNameChange, newPhone, handlePhoneChange}) => {
+  return(
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange}/>
+      </div>
+      <div>
+        number: <input value={newPhone} onChange={handlePhoneChange}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({persons, searchInput}) =>{
+  return(
+    <ul>
+      {persons
+        .filter(person => person.name.toLowerCase().includes(searchInput.toLowerCase()))
+        .map(person =>(
+          <Person key={person.name} name={person.name} phone={person.phone}/>
+        )
+      )}
+    </ul>
+  )
+}
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [searchInput, setSearchInput] = useState('')
 
-  return <Course course={courses} />
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    let ok = true
+    persons.forEach(person =>{
+      if(person.name === newName)
+        ok = false
+    })
+
+    if(ok === false){
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+    }else{
+      const personObject = {
+        name: newName,
+        phone: newPhone
+      }
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewPhone('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchInput(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter searchInput={searchInput} handleSearchChange={handleSearchChange}/>
+      
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newPhone={newPhone} handlePhoneChange={handlePhoneChange}
+      />
+      <h2>Numbers</h2>
+      <Persons
+        persons={persons} searchInput={searchInput}
+      />
+    </div>
+  )
 }
 
 export default App
